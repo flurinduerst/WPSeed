cdc38a1f03b7d66300832256e78f7274833b7b39
Flurin Dürst <mail@flurin.me>
2019-02-26 17:26:48 +0100

    update functions inc-file, add acf-gutenberg presets

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * • general functions like replacements, translations
  *
  * @author      Flurin Dürst
- * @version     2.0
+ * @version     2.1
  * @since       WPSeed 0.1
  *
  */
@@ -15,26 +15,31 @@
 /*==================================================================================
   DEVELOPER TOOLKIT
 ==================================================================================*/
+// functions used for development purposes like debuggers sanitizers or IP-checks
 require('functions/functions-dev.php');
-
 
 
 /*==================================================================================
   WP SETUP & SETTINGS
 ==================================================================================*/
+// WP-setup and preferences + general settings, filters, theme support & more
 require('functions/functions-setup.php');
 require('functions/functions-settings.php');
 
 
 
 /*==================================================================================
+  GUTENBERG ACF
 ==================================================================================*/
+// functions for any Custom Gutenberg Blocks created with ACF
+require('functions/functions-gutenberg.php');
 
 
 
 /*==================================================================================
   CUSTOM
 ==================================================================================*/
+// custom functions like shortcodes should be added here
 require('functions/functions-custom.php');
 
 
---
+++ functions/functions-gutenberg.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Contains the functions for any Custom Gutenberg Blocks created with ACF
+ * The Block "Employees" is here as a preset for custom ACF-Blocks.
+ * Requires ACF Version 5.8+
+ *
+ * @author      Flurin Dürst
+ * @version     1.0.0
+ * @since       WPSeed 2.0.0
+ *
+ *
+ */
+
+ /*==================================================================================
+   Employees, Preset ACF Gutenberg Block
+ ==================================================================================*/
+
+ /* Register ACF Block
+ /––––––––––––––––––––––––*/
+ if( function_exists('acf_register_block') ) {
+
+ 	$result = acf_register_block(array(
+ 		'name'				     => 'employees',
+ 		'title'				     => __('Mitarbeiter'),
+ 		'description'		   => __('Custom Block für Mitarbeiter'),
+ 		'render_callback'	 => 'WPSeed_Gutenblock_Employees',
+ 		'category'		     => 'common', // common, formatting, layout, widgets, embed
+ 		'icon'			       => 'admin-users',
+ 		'keywords'		     => ['employee', 'mitarbeiter', 'worker']
+ 	));
+ }
+
+ /* Render Block
+ /––––––––––––––––––––––––*/
+ function WPSeed_Gutenblock_Employees() {
+
+ 	// Get Vars
+ 	$name = get_field('name');
+ 	$image = get_field('image');
+ 	$title = get_field('title');
+ 	$email = get_field('email');
+
+  // Return HTML
+ 	?>
+ 	<div class="employee">
+     <? echo wp_get_attachment_image( $image['ID'], 'thumbnail', "", ['class' => 'modernizr-of']); ?>
+     <p><b><?= $name ?></b></p>
+     <p><?= $title ?></p>
+     <p><a href="mailto:<?= $email ?>"><?= $email ?></a></p>
+   </div>
+ 	<?
+
+ }
