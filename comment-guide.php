cd553049124541072ecc2a6f0994489debbc39f7
Flurin Dürst <mail@flurin.me>
2019-03-01 09:33:40 +0100

    add the comment guideline

---
+++ comment-guide.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file's purpose is only to show how code is
+ * structured using comments in WPSeed
+ * @author      Flurin Dürst
+ * @version     1.0
+ * @since       WPSeed 2.0
+ *
+ */
+
+
+/*=======================================================
+Table of Contents (2 breaks before)
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+ 1.0 LOREM
+ 2.0 IPSUM
+ 3.0 AMET
+ 4.0 CONSTRUCTOR
+ =======================================================*/
+
+
+
+/*==================================================================================
+  1.0 MAIN LEVEL (uppercase)
+==================================================================================*/
+
+
+/* first Sub-Level
+/––––––––––––––––––––––––*/
+// second sub-level
+// » https://url-goes-here-if-needed.com
+if (code) :
+  echo 'code goes here';
+endif;
+
+// second sub-level
